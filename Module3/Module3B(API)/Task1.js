7c843d29044a013cebc1e3dca2c699bff207b52e
Jephthah <sundaychimezie96@gmail.com>
2023-07-31 07:39:24 +0100

    Repo Update

---
+++ Module3/Module3B(API)/Task1.js
@@ -0,0 +1,10 @@
+//Calculate the sum of numbers within an array
+
+const array=[10,12,13,24,34,56,5,6,67,65]
+
+function sum(a,b){
+    const addition = a + b
+    console.log(addition)
+}
+
+sum(array[3],array[5])
